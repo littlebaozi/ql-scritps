c3e0b9a061efa984266e6181aa00e732cb4a8f81
little_baozi <15085513+littlebaozi@users.noreply.github.com>
2022-07-23 14:15:45 +0000

    julang签到

---
+++ julang_cloud_sign.js
@@ -0,0 +1,47 @@
+const scriptName = "JulangCloud";
+let magicJS = MagicJS(scriptName, "INFO");
+const $ = new Env("JulangCloud签到");
+const notify = $.isNode() ? require('./sendNotify') : '';
+
+//签到
+function SignIn(cookie) {
+  return new Promise((resolve) => {
+    let options = {
+      url: 'https://julang.site/user/checkin',
+      headers: {
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+        "Connection": "keep-alive",
+        "Cookie": cookie,
+        "Host": "julang.site",
+        "Referer": "https://julang.site/user",
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.51",
+      },
+    };
+    $.post(options, (err, resp, data) => {
+      try {
+        if (err) {
+          console.log('签到请求失败！！\n');
+          console.log(err);
+        } else {
+          console.log(data);
+          data = JSON.parse(data);
+          notify.sendNotify(scriptName, `JuLang签到成功：${data.msg}`);
+          console.log(`签到成功！！\n`);
+        }
+      } catch (e) {
+        $.logErr(e, resp);
+      } finally {
+        resolve();
+      }
+    });
+  });
+}
+
+try {
+  result = await SignIn(process.env.JULANG_COOKIE)
+} catch (err) {
+  console.log(`执行任务出现异常：${err}`);
+}
